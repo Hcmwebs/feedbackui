46f0ae6827ce73d3c8cd761dcd534e36dee6a7c3
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-05-08 02:01:17 +1200

    finished the styling of card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -7,14 +7,14 @@ const Card = () => {
 		<>
 			<main>
 				<div className='card'>
-					<BsXCircle className='icon-close'/>
+					<BsXCircle className='icon-close' />
 					<p>How satisfied are you using our invoicing feature?</p>
 					<div className='rating-btns'>
 						<button className='btn'>1</button>
 						<button className='btn'>2</button>
 						<button className='btn'>3</button>
 						<button className='btn'>4</button>
-						<button className='btn'>5</button>
+						<button className='btn active'>5</button>
 					</div>
 					<div className='rating-info'>
 						<small>
@@ -28,8 +28,8 @@ const Card = () => {
 						<input type='checkbox' />
 						<p>I don't use the invoicing feature.</p>
 					</div>
-					<div className='divider'> hr </div>
-					<button className='btn'>
+					<div className='divider'></div>
+					<button className='btn btn-next'>
 						Next <BsArrowRight />
 					</button>
 				</div>
