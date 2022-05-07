33f7f5e09265e1c61c47535a1f514b1acdbf07ee
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-05-08 00:14:54 +1200

    first commit

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { BsArrowRight, BsXCircle } from 'react-icons/bs'
+import './index.scss'
+
+const Card = () => {
+	return (
+		<>
+			<main>
+				<div className='card'>
+					<BsXCircle className='icon-close'/>
+					<p>How satisfied are you using our invoicing feature?</p>
+					<div className='rating-btns'>
+						<button className='btn'>1</button>
+						<button className='btn'>2</button>
+						<button className='btn'>3</button>
+						<button className='btn'>4</button>
+						<button className='btn'>5</button>
+					</div>
+					<div className='rating-info'>
+						<small>
+							Extremely <br /> dissatisfied
+						</small>
+						<small>
+							Extremely <br /> satisfied
+						</small>
+					</div>
+					<div className='input-group'>
+						<input type='checkbox' />
+						<p>I don't use the invoicing feature.</p>
+					</div>
+					<div className='divider'> hr </div>
+					<button className='btn'>
+						Next <BsArrowRight />
+					</button>
+				</div>
+			</main>
+		</>
+	)
+}
+
+export default Card
---
+++ src/Components/Title/Title.js
@@ -0,0 +1,14 @@
+import './index.scss'
+import React from 'react'
+
+const Title = () => {
+	return (
+		<>
+			<header>
+				<h1 className='title'>Heading goes here!</h1>
+			</header>
+		</>
+	)
+}
+
+export default Title
