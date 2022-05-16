9d5a1b25c0d896f13b2dce68e9c84e17593c56fb
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-05-17 01:14:09 +1200

    completed the functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.scss'
 import Form from './Components/Form/Form'
+import Title from './Components/Title/Title'
 
 const App = () => {
 	return (
-		<>
+		<div className='container'>
+			<Title />
 			<Form />
-		</>
+		</div>
 	)
 }
 
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,30 +1,86 @@
 import React, { useState } from 'react'
-import Button from '../Button/Button'
-import Card from '../Card/Card'
-import Title from '../Title/Title'
+import { BsStarFill, BsArrowRight } from 'react-icons/bs'
+import data from '../../db/data'
 import './index.scss'
 
 const Form = () => {
 	const [activeValue, setActiveValue] = useState('')
-	const [hoverValue, setHoverValue] = useState('')
+	const [selectedValue, setSelectedValue] = useState([])
+
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		setHoverValue('')
-		setActiveValue('')
-		console.log('submitted')
+		if (activeValue) {
+			const newValue = { id: new Date().getTime().toString(), activeValue }
+			setSelectedValue((selectedValue) => {
+				return [...selectedValue, newValue]
+			})
+			setActiveValue('')
+			setTimeout(() => {
+				window.location.reload()
+			}, 5000)
+		} else {
+			alert('Please select a rating')
+		}
 	}
 	return (
 		<>
-			<form className='form' onSubmit={handleSubmit}>
-				<Title />
-				<Card
-					activeValue={activeValue}
-					setActiveValue={setActiveValue}
-					hoverValue={hoverValue}
-					setHoverValue={setHoverValue}
-				/>
-				<Button />
-			</form>
+			<main>
+				<form className='form' onSubmit={handleSubmit}>
+					<div className='form-body'>
+						<h2>
+							How satisfied are you using <br /> our invoicing feature?
+						</h2>
+						<div className='ratings'>
+							{data.map((item) => {
+								const { id, name, value } = item
+
+								return (
+									<label htmlFor={id} key={id}>
+										<BsStarFill
+											className={`${
+												value <= activeValue
+													? 'icon-star isActive'
+													: 'icon-star'
+											}`}
+										/>
+										<input
+											name={name}
+											type='button'
+											id={id}
+											value={value}
+											onClick={() => setActiveValue(value)}
+										/>
+									</label>
+								)
+							})}
+						</div>
+
+						<div className='ratings'>
+							<small>
+								Extremely <br /> dissatisfied
+							</small>
+							<small>
+								Extremely <br /> satisfied
+							</small>
+						</div>
+					</div>
+					<button className='btn'>
+						Next
+						<BsArrowRight />
+					</button>
+				</form>
+				{selectedValue.map((item) => {
+					const { id, activeValue: value } = item
+					return (
+						<article className='card' key={id}>
+							<p>
+								Thank you very much. <br /> Your <b>{value} star rating </b>
+								will be <br /> used to improve our services.
+							</p>
+						</article>
+					)
+				})}
+			</main>
 		</>
 	)
 }
